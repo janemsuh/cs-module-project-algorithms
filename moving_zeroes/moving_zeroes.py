636037a7205f5db0deec3bce9607507d02382a16
Jane Suh <sourcherryyogurt@gmail.com>
2020-07-19 15:55:07 -0400

    Completed moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,10 +3,25 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    zeroes_arr = []
+    for idx, val in enumerate(arr):
+        # add zero value indexes in arr to zeroes_arr
+        if val == 0:
+            zeroes_arr.append(idx)
+    # print(zeroes_arr)
+    
+    for idx, val in enumerate(zeroes_arr):
+        # remove zero from arr based on index (val in zeroes_arr = index of 0 in arr) saved in zeroes_arr
+        # must subtract idx after each successive zero is moved
+        arr.pop(val - idx)
+        # print(f"val: {val}")
+        # print(f"idx: {idx}")
+        # print(f"val - idx: {val - idx}")
 
-    pass
+        # add zero to the end of arr
+        arr.append(0)
 
+    return arr
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
